9b32d5574527b6a403b5c639051f53b139aeda24
zainul abidin <bidingurusmk@gmail.com>
2023-10-05 13:34:55 +0700

    rev

--- lib/gambar.dart
+++ lib/gambar.dart
@@ -6,6 +6,11 @@ class GambarWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => {},
+          child: Icon(Icons.message),
+          backgroundColor: Colors.blue[900],
+        ),
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text("Home"),
